1d55403f33c5c4c7f7fdfd1f449921cfa9d9dc45
danfengl <danfengl@vmware.com>
2022-12-08 03:39:58 +0000

    debugcontext deadline exceeded issue
    Signed-off-by: danfengl <danfengl@vmware.com>

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -23,13 +23,30 @@ do
 	STATUS=`etcdctl get /kibishii/ops/$OPID --endpoints=http://etcd-client:2379 --print-value-only | jq ".status" | sed -e 's/"//g'`
 	NODES_COMPLETED=`etcdctl get /kibishii/ops/$OPID --endpoints=http://etcd-client:2379 --print-value-only | jq ".nodesCompleted" | sed -e 's/"//g'`
 done
-if [ "$NODES_COMPLETED" -ne "$NODES" ] 
+if [ "$NODES_COMPLETED" != "$NODES" ] 
 then
 	STATUS="failed"
 fi
 echo $STATUS
 if [ "$STATUS" = 'success' ]
 then
+    nodes=`etcdctl get /kibishii/nodes/ --prefix --endpoints=http://etcd-client:2379 | grep ^kibishii-deployment`
+    for node in $nodes
+    do
+        results=`etcdctl get /kibishii/results/$OPID/$node --endpoints=http://etcd-client:2379 --print-value-only | jq ".missingDirs,.missingFiles"`
+        for result in $results
+        do
+            if [ -z $result ]; then
+                exit 2
+            fi
+            if [ "$result" !=  '0' ]; then
+                exit $result
+            fi
+        done
+    done
 	exit 0
 fi
+
 exit 1
+
+
